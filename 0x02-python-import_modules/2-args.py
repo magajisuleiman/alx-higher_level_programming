754e1ede7f6d41e82ae7a6533072b379bdddc069
magajisuleiman <magajisuleiman1203@gmail.com>
2023-06-08 13:13:04 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    length = len(sys.argv) - 1
+    if length == 0:
+        print("{} arguments.".format(0))
+    elif length == 1:
+        print("{} arguments:".format(1))
+    else:
+        print("{} arguments:".format(length))
+    for i in range(length):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
