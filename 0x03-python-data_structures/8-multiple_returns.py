903f1d96262af6e46953f18215e16d641bab2317
magajisuleiman <magajisuleiman1203@gmail.com>
2023-06-11 09:28:42 -0700

    8. More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    counter = 0
-    for i in sentence:
-        counter += 1
-        if sentence == "":
-            sentence = None
-        else:
-            sentence = sentence[0]
-
-    return (counter, sentence)
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
