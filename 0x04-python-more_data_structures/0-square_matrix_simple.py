114b0e5d3d4c36d702546befb3af10578fa69cbb
magajisuleiman <magajisuleiman1203@gmail.com>
2023-06-14 19:06:03 -0700

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    matrix_ = matrix.copy()
+
+    for i in range(len(matrix)):
+        matrix_[i] = list(map(lambda x: x**2, matrix[i]))
+
+    return (matrix_)
+
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+This repo contains all the solution to 0x04. Python - More Data Structures: Set, Dictionary
