3b523864e1f9ee0018c993700dd5dad97c30dc89
magajisuleiman <magajisuleiman1203@gmail.com>
2023-06-27 14:47:19 -0700

    removed file

--- 100-safe_print_integer_err.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-def safe_print_integer_err(value):
-    try:
-        print("{:d}".format(value), end="")
-        return True
-    except Exception:
-        print("Exception: Unknown format code 'd' for object of type 'str'".format())
-        return False
