ad33004b8d993bccb0a907ba16bfe234ca8faa26
magajisuleiman <magajisuleiman1203@gmail.com>
2023-07-03 06:21:26 -0700

    A class that defines a simple rectanglle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+class Rectangle:
+    """
+    A class representing a rectangle.
+
+    Attributes:
+    None
+
+    Methods:
+    None
+    """
+
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+This directory contains all the soulutions to alx project 0x08. Python - More Classes and Objects
