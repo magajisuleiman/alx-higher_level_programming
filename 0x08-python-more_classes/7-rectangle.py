3977aeb49f62fa6f69bd03baab77ae1c7a8c4b61
magajisuleiman <magajisuleiman1203@gmail.com>
2023-07-03 20:12:31 -0700

    7. Change representation

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+"""
+Module 2-rectangle
+Contains class Rectangle with private attribute width and height,
+and public area and perimeter methods
+"""
+
+
+class Rectangle:
+    """
+    Defines class rectangle with private attribute width and height
+
+    Args:
+        width (int): width
+        height (int): height
+
+    Functions:
+        __init__(self, width, height)
+        width(self)
+        width(self, value)
+        height(self)
+        height(self, value)
+        area(self)
+        perimeter(self)
+    """
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """Class Constructor"""
+        self.width = width
+        self.height = height
+        type(self).number_of_instances += 1
+
+    @property
+    def width(self):
+        """getter function for width"""
+        return self.__width
+
+    @property
+    def height(self):
+        """Getter returns height"""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """setter funtion for width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """setter function for height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return width * height"""
+        areaRectangle = self.__width * self.__height
+        return areaRectangle
+
+    def perimeter(self):
+        """Return 2*width + 2*height (or return 0 if width or height is 0)"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            perimeterRectangle = 2 * (self.__width + self.__height)
+            return perimeterRectangle
+
+    def __str__(self) -> str:
+        """str formated output"""
+        if self.__height == 0 or self.__width == 0:
+            return ""
+        pic = "\n".join(
+            [self.print_symbol * self.__width for i in range(self.__height)]
+        )
+        return pic
+
+    def __repr__(self) -> repr:
+        """repr formated output to oval()"""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """Destructor"""
+        message = "Bye rectangle..."
+        print("{}".format(message))
+        type(self).number_of_instances -= 1
