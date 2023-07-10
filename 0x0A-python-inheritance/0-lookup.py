13f64ba6f9257d36e3282b10ae994726e3974439
magajisuleiman <magajisuleiman1203@gmail.com>
2023-07-10 11:15:48 -0700

    0. Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+def lookup(obj):
+    """A function that returns the lists of attr/method"""
+
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+This directory contains all the solutions to ALX 0x0A. Python - Inheritance task and solutions 
