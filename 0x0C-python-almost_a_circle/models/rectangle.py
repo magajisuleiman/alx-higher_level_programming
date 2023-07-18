714a79b1b7fbcb28373be2b5ba08c17f4b35c1e5
magajisuleiman <magajisuleiman1203@gmail.com>
2023-07-18 06:37:56 -0700

    grand finale

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -103,11 +103,12 @@ class Rectangle(Base):
         """Return area"""
         return self.__width * self.__height
 
-    def display(self):
+     def display(self):
         """Print to stdout a rectangle using #'s"""
-        print("\n".join(" " * self.__x + "#" * self.__width for _ in range(self.__y + self.__height)))
-
-
+        print("\n" * self.__y +
+              "\n".join(" " * self.__x + "#" * self.__width
+                        for i in range(self.__height)))
+              
     def __str__(self):
         """Prints [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
         return "[{:s}] ({:d}) {:d}/{:d} - {:d}/{:d}".format(
