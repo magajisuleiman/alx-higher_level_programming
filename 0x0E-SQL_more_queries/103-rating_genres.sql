b2e8ebf1a75e0ffee1983d9d1b6c66585fcb4a0a
magajisuleiman <magajisuleiman1203@gmail.com>
2023-08-17 02:40:13 +0100

    20. Best genre

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,10 @@
+-- the database name will be passed as an argument of the mysql command
+
+    SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS 'rating'
+      FROM tv_genres
+INNER JOIN tv_show_genres
+        ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_show_ratings
+        ON tv_show_ratings.show_id = tv_show_genres.show_id
+  GROUP BY name
+  ORDER BY rating DESC;
