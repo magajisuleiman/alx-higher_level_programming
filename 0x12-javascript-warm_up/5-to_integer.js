15ee741a4a2d2c38ec3198704e7fd30f9586f36c
magajisuleiman <magajisuleiman1203@gmail.com>
2023-09-11 22:52:08 +0100

    5. An Integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const { argv } = require('process');
+
+const firstValue = parseInt(argv[2], 10);
+
+if (isNaN(firstValue)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number ${firstValue}`);
+}
