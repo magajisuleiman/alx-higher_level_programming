0837c33b3eed6d87c2e7ff48adb8f7641d060278
magajisuleiman <magajisuleiman1203@gmail.com>
2023-09-12 03:15:45 +0100

    updated

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
 const { argv } = require('process');
 
-if (isNaN(argv[2])) {
+const size = parseInt(process.argv[2], 10);
+
+if (isNaN(size)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < argv[2]; i++) {
+  for (let i = 0; i < size; i++) {
     let line = '';
-    for (let j = 0; j < argv[2]; j++) {
+    for (let j = 0; j < size; j++) {
       line += 'X';
     }
     console.log(line);
