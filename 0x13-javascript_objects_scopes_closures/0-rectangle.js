abb746ab80bbdcc3fdedf499a2305ea4efd0c0d7
magajisuleiman <magajisuleiman1203@gmail.com>
2023-09-12 20:53:50 +0100

    0. Rectangle #0

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (_w, h) {
+    this.width = this.width;
+    this.height = this.h;
+  }
+}
+
+
+const r1 = new Rectangle();
+console.log(r1);
+console.log(r1.constructor);
---
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -0,0 +1,1 @@
+This directory contain all the solutions to 0x13. JavaScript - Objects, Scopes and Closures
