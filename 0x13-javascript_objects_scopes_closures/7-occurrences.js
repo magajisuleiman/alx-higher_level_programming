b6d81449a0206d75ad5ed4931546edbddbc1615c
magajisuleiman <magajisuleiman1203@gmail.com>
2023-09-13 01:54:14 +0100

    updated

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,9 @@
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (const i in list) {
+    if (list[i] === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
