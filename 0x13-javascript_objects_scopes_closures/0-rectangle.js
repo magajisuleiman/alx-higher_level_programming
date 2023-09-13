a8095261821ecb5bfe5b06f1994cf026ff891540
magajisuleiman <magajisuleiman1203@gmail.com>
2023-09-13 01:18:54 +0100

    1. Rectangle #1

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,13 +1,3 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (_w, h) {
-    this.width = this.width;
-    this.height = this.h;
-  }
-}
-
-
-const r1 = new Rectangle();
-console.log(r1);
-console.log(r1.constructor);
+module.export = class Rectangle {};
