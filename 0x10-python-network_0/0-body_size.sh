ed34a0b73240e23681113afe5f0b19f5bf1e2a8e
magajisuleiman <magajisuleiman1203@gmail.com>
2023-09-29 00:09:27 +0100

    updated

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 # Display size of body of response; Usage: ./0-body_size.sh 0.0.0.0:5000
 
-URL="$1"
 headers_file=$(mktemp)
-curl -sI "$URL" -o "$headers_file"
-response_size=$(grep 'Content-Length:' "$headers_file" | cut -f2 -d' ')
-echo ${response_size}
+curl -sI "$1" -o "$headers_file" | grep 'Content-Length:' "$headers_file" | cut -f2 -d' '
